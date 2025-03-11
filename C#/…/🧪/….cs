3e942246267139894eb9108068ebcc7aded3315d
LapysDev <lapysdev@gmail.com>
2025-03-11 10:13:09 +0000

    …

---
+++ C#/…/🧪/….cs
@@ -0,0 +1,6 @@
+#nullable enable annotations
+
+public static class Program {
+  /* Main --> del ….exe ….rsp & cls && (for %I in (*.dll) do @echo /r:"%I") > ….rsp && csc /out:….exe @….rsp /t:exe ….cs && ….exe & del ….exe ….rsp */
+  static void Main(string[] arguments) {}
+}
