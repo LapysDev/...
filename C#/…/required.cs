74195e6186611e468e1725d8bf43e5628c2290ed
LapysDev <lapysdev@gmail.com>
2025-04-03 22:32:12 +0100

    …

---
+++ C#/…/required.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Diagnostics.CodeAnalysis /* ⟶ `required` @ `https://github.com/dotnet/core/issues/8016` `https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/required` */ {
+  [System.AttributeUsage(System.AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+  public sealed class SetsRequiredMembersAttribute : System.Attribute {}
+}
+
+namespace System.Runtime.CompilerServices /* ⟶ `required` @ `https://github.com/dotnet/core/issues/8016` `https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/required` */ {
+  public class CompilerFeatureRequiredAttribute : System.Attribute {
+    public CompilerFeatureRequiredAttribute(string name) {}
+  }
+
+  public class RequiredMemberAttribute : System.Attribute {}
+}
--- C#/…/🧪/….cs
+++ C#/…/🧪/….cs
@@ -1,6 +1,6 @@
 #nullable enable annotations
 
 public static class Program {
-  /* Main --> del ….exe ….rsp & cls && (for %I in (*.dll) do @echo /r:"%I") > ….rsp && csc /out:….exe @….rsp /t:exe ….cs && ….exe & del ….exe ….rsp */
+  /* Main --> del ….exe ….rsp & cls && (for %I in (*.dll) do @echo /r:"%I") > ….rsp && csc /out:….exe @….rsp /t:exe ../required.cs ….cs && ….exe & del ….exe ….rsp */
   static void Main(string[] arguments) {}
 }
