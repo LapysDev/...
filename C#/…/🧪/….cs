133ef89c20f5aa38c0ecc4fb9519bbd470d949bd
LapysDev <lapysdev@gmail.com>
2025-04-05 19:40:47 +0100

    …

--- C#/…/🧪/….cs
+++ C#/…/🧪/….cs
@@ -1,6 +1,13 @@
 #nullable enable annotations
 
 public static class Program {
+  public class Event : System.EventArgs {
+    public required object? data  =  null;
+    public ref      object? value => ref this.data;
+  }
+
   /* Main --> del ….exe ….rsp & cls && (for %I in (*.dll) do @echo /r:"%I") > ….rsp && csc /out:….exe @….rsp /t:exe ../required.cs ….cs && ….exe & del ….exe ….rsp */
-  static void Main(string[] arguments) {}
+  static void Main(string[] arguments) {
+    new Event() {data = null};
+  }
 }
