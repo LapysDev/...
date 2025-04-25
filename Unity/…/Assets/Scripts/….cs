2f600e6b4c3ae16c50fc4dc5cf10953a8a2b5ae9
LapysDev <lapysdev@gmail.com>
2025-04-25 22:47:00 +0100

    …

--- Unity/…/Assets/Scripts/….cs
+++ Unity/…/Assets/Scripts/….cs
@@ -1,37 +1,14 @@
-namespace System.Runtime.CompilerServices {
-  [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Interface | System.AttributeTargets.Struct, Inherited = false)]
-  [System.Diagnostics.DebuggerNonUserCode]
-  [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-  internal sealed class CollectionBuilderAttribute : System.Attribute {
-    public System.Type BuilderType { get; }
-    public string      MethodName  { get; }
-
-    public CollectionBuilderAttribute(System.Type builderType, string methodName) {
-      this.BuilderType = builderType;
-      this.MethodName  = methodName;
-    }
-  }
-}
-
-/* … */
 public static class Program {
   [UnityEngine.RuntimeInitializeOnLoadMethod]
-  private static void Main() {
-    new UnityEngine.GameObject("…", typeof(ProgramBehaviour));
-  }
+  private static void Main() => new UnityEngine.GameObject("…", typeof(ProgramBehaviour));
 }
 
 internal sealed class ProgramBehaviour : UnityEngine.MonoBehaviour {
-  public struct B {
-    public int this[int index] => 42;
-    public int this[System.Range _] => 42;
+  /* … */
+  private void Awake() {
+    /* Do something… */
   }
 
-  private static void Awake() {
-    System.Console.WriteLine(new B()[0..1]);
-     System.Collections.Generic.List<int> list = [1, 2, 3];
-  }
-
-  private static void FixedUpdate() {}
-  private static void OnDestroy  () {}
+  private void FixedUpdate() {}
+  private void OnDestroy  () {}
 }
