53b99552de63bc721b360775ed9e3400fbb72dea
LapysDev <lapysdev@gmail.com>
2025-06-14 22:42:26 +0100

    …

--- C++/…/bit-manipulation.cpp
+++ C++/…/bit-manipulation.cpp
@@ -3,7 +3,7 @@ constexpr inline int clear_bit(int const value, std::size_t const index) noexcep
 }
 
 constexpr inline int get_bit(int const value, std::size_t const index) noexcept {
-  return ((value >> index) & 1u) ? 1u : 0u;
+  return (value >> index) & 1u ? 1u : 0u;
 }
 
 constexpr inline int get_high(int const value, std::size_t const bitcount) noexcept {
@@ -15,7 +15,7 @@ constexpr inline int get_low(int const value, std::size_t const bitcount) noexce
 }
 
 constexpr inline bool is_power_of_two(int const value) noexcept {
-  return (value ^ 0) && ((value & (value - 1)) == 0);
+  return (value ^ 0) && (0 == value & (value - 1));
 }
 
 constexpr inline int ones_complement(int const value) noexcept {
