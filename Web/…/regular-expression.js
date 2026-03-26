c28e623fdf7ae93fb74c14bdfbad5879579452f7
LapysDev <lapysdev@gmail.com>
2026-03-26 22:36:59 +0000

    …

--- Web/…/regular-expression.js
+++ Web/…/regular-expression.js
@@ -5,11 +5,11 @@
 "A,B"  .replace(/,/,                     "$'");         // --> "ABB"
 "A,B"  .replace(/,/,                     "$$");         // --> "A$B"
 "A,B"  .replace(/(?<comma>,)/,           "1$<comma>2"); // --> "A1,2B"
-",ABC,".replace(/([A-Z])([A-Z])([A-Z])/, function(match, groupA, groupB, groupC, index, string) {
-  void groupA, groupB, groupC; // --> "A", "B", "C"
-  void index;                  // --> 1
-  void match;                  // --> "ABC"
-  void string;                 // --> ",ABC,"
+",ABC,".replace(/([A-Z])([A-Z])([A-Z])/, function(match, $1, $2, $3, index, string) {
+  void $1, $2, $3; // --> "A", "B", "C"
+  void index;      // --> 1
+  void match;      // --> "ABC"
+  void string;     // --> ",ABC,"
 
   return ""
 }) // --> ",,"
