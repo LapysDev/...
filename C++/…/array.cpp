8ac5e9eaac9a96c6aebfbc15c56f139836a5e605
LapysDev <lapysdev@gmail.com>
2026-04-07 21:49:40 +0100

    …

--- C++/…/array.cpp
+++ C++/…/array.cpp
@@ -1,125 +1,77 @@
-#include <ciso646> // → not
-#include <climits> // → SIZE_MAX
-#include <cstddef> // → NULL; std::byte, std::size_t
-#include <cstdio>  //
-#include <memory>  // → std::align(…)
-#include <new>     // → delete, new
+#include <cstddef>
+#include <cstdio>
+#include <new>
+#include <stdint.h>
 
-/* … → Assumed `T` is not implicit-lifetime or transparently replaceable: no optimizations */
-template <typename T>
-struct array {
+/* ... ->> Constant-time initialization is the user’s responsibility i.e. `array<…, FIXED_SIZE_ONLY>` */
+struct array_internals {
+  template <typename, std::size_t>
+  friend struct array;
+
   private:
-    void       *value;    // Interface for accessing elements
-    std::size_t capacity; // — followed by opaque storage for allocating elements
-    std::size_t length;   //   `struct { T*[]; T[]; }`
+    static std::size_t count;
+    static void const *recent;
+};
+  std::size_t array_internals::count  = 0u;
+  void const *array_internals::recent = NULL;
 
-  public:
-    array() :
-      value   (NULL),
-      capacity(0u),
-      length  (0u)
-    {}
+template <typename base, std::size_t initial = 0u>
+struct array {
+  enum { minimum = sizeof(base) < sizeof(std::size_t) ? (initial - 1u) + (sizeof(std::size_t) / sizeof(base)) : initial }; // --> std::size_t
+  private:
+    union param { unsigned char unassigned : 1; base _; };
+    union storage {
+      private:
+        unsigned char _ : 1;
+        base value;
 
-    ~array() {
-      ::operator delete(this -> value);
+        void initialize(base const *const value = NULL) {
+          uintptr_t const address = reinterpret_cast<uintptr_t>(static_cast<void const*>(this));
+          uintptr_t const recent  = reinterpret_cast<uintptr_t>(array_internals::recent);
 
-      this -> capacity = 0u;
-      this -> length   = 0u;
-      this -> value    = NULL;
-    }
+          // ...
+          NULL == value ? std::printf(array_internals::count == minimum - 1u ? "+" : "-") : std::printf("%i", (int) *value);
+          if (array_internals::count == minimum - 1u) {
+            // PUT INDEX TO HEAP MEMORY HERE @ `storage* this`
+            (void) std::printf("{%zu}" "\r\n", array_internals::count);
+          } else { std::printf("{%zu}" " ", array_internals::count); }
+          std::printf(minimum == 1 ? "\r\n" : "");
 
-    // …
-    bool add(T const& object) {
-      T *data;
+          if (array_internals::count >= minimum - 0u || sizeof(base) < (address < recent ? recent - address : address - recent))
+          array_internals::count = 0u;
 
-      // …
-      if (this -> capacity == this -> length) {
-        std::size_t capacity = 1u;
-
-        // …
-        for (std::size_t rate = 1u; capacity < this -> capacity; ) {
-          rate     *= (capacity > SIZE_MAX - (capacity / rate)) + 1u;
-          capacity += capacity / rate;
+          array_internals::count++;
+          array_internals::recent = this;
         }
 
-        if (not this -> reserve(capacity))
-        return false;
-      }
+      public:
+        storage()                  : _() { this -> initialize(NULL); }
+        storage(base const& value) : _() { this -> initialize(reinterpret_cast<base const*>(&reinterpret_cast<unsigned char const&>(value))); }
+    };
 
-      data = this -> data();
+    static union storage const *recent;
 
-      if (NULL == data)
-      return false;
+  public:
+    union storage _[minimum];
+    bool add(union param const& element1 = param(), union param const& element2 = param(), union param const& element3 = param());
+};
 
-      static_cast<T**>(this -> value)[this -> length] = ::new (data + this -> length) T(object);
-      this -> length                                 += 1u;
-
-      return true;
-    }
-
-    T* data() const {
-      std::size_t capacity = (this -> capacity * sizeof(T)) + (alignof(T) > sizeof(T) ? alignof(T) : sizeof(T));
-      void       *value    = static_cast<T**>(this -> value) + this -> capacity;
-
-      // Unusable as an array of `T`
-      return static_cast<T*>(std::align(alignof(T), sizeof(T), value, capacity));
-    }
-
-    bool reserve(std::size_t count) {
-      struct {
-        union { std::size_t capacity, length; };
-        union { void *value; T **elements; };
-      } allocation, buffer, information = {{this -> length}, {this -> value}};
-
-      // …
-      if (count <= this -> capacity)
-      return true;
-
-      allocation.capacity = (count * sizeof(T)) + (alignof(T) > sizeof(T) ? alignof(T) : sizeof(T)) + (count * sizeof(T*));
-      allocation.value    = ::operator new(allocation.capacity, static_cast<std::align_val_t>(alignof(T*)), std::nothrow);
-
-      if (NULL == allocation.value) {
-        ::operator delete(allocation.value);
-        return false;
-      }
-
-      buffer.capacity = allocation.capacity                       - (count * sizeof(T*));
-      buffer.value    = static_cast<std::byte*>(allocation.value) + (count * sizeof(T*));
-      buffer.value    = std::align(alignof(T), sizeof(T), buffer.value, buffer.capacity);
-
-      if (NULL == buffer.value) {
-        ::operator delete(allocation.value);
-        return false;
-      }
-
-      allocation.elements = ::new (allocation.value) T*[count];
-      this -> capacity    = 0u;   // Prevent `array<T>` being observed (meaningfully)
-      this -> length      = 0u;   // — as a side-effect of reconstructing its elements
-      this -> value       = NULL; //
-
-      if (NULL != information.value) {
-        for (std::size_t index = 0u; index != information.length; ++index)
-        allocation.elements[index] = ::new (static_cast<T*>(buffer.value) + index) T(
-          *(static_cast<T**>(information.value)[index])
-        );
-
-        // Maintain LIFO order
-        for (std::size_t index = information.length; index; )
-        static_cast<T**>(information.value)[--index] -> ~T();
-      }
-
-      ::operator delete(information.value);
-      this -> capacity = count;
-      this -> length   = information.length;
-      this -> value    = allocation .value;
-
-      return true;
-    }
-
-    T& operator [](std::size_t index) const {
-      return *(static_cast<T**>(index < this -> length ? this -> value : NULL)[index]);
-    }
+template <typename base>
+struct array<base, 0u> : array_internals {
+  public: union { struct array_internals __; base *_; };
 };
 
 /* Main */
-int main(int, char*[]) /* noexcept */ {}
+int main(void) {
+  array<long, 0u> a;
+  array<long, 3u> b[] = {{1, 2, 3}, {4, 5, 6}};
+  array<unsigned char, 3u> c[] = {{10, 20, 30}, {40, 50, 60}, {70}, {80}};
+  array<short, 1u> d[] = {{100}, {200}, {300}};
+  array<long long, 1u> e[] = {{400}, {500}, {600}};
+
+  (void) a;
+  (void) b;
+  (void) c;
+  (void) d;
+  (void) e;
+}
