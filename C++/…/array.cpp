29f9e484ac217a3068acbcf442bc57bbb8f88581
LapysDev <lapysdev@gmail.com>
2026-04-14 02:11:38 +0100

    …

--- C++/…/array.cpp
+++ C++/…/array.cpp
@@ -2,76 +2,71 @@
 #include <cstdio>
 #include <new>
 #include <stdint.h>
+#include <utility>
 
 /* ... ->> Constant-time initialization is the user’s responsibility i.e. `array<…, FIXED_SIZE_ONLY>` */
-struct array_internals {
-  template <typename, std::size_t>
-  friend struct array;
-
-  private:
-    static std::size_t count;
-    static void const *recent;
-};
-  std::size_t array_internals::count  = 0u;
-  void const *array_internals::recent = NULL;
-
-template <typename base, std::size_t initial = 0u>
+template <typename T, std::size_t N>
 struct array {
-  enum { minimum = sizeof(base) < sizeof(std::size_t) ? (initial - 1u) + (sizeof(std::size_t) / sizeof(base)) : initial }; // --> std::size_t
-  private:
-    union param { unsigned char unassigned : 1; base _; };
-    union storage {
-      private:
-        unsigned char _ : 1;
-        base value;
+  struct elements {
+    union automatic {
+      union element {
+        char keep_uninitialized;
+        T    value;
 
-        void initialize(base const *const value = NULL) {
-          uintptr_t const address = reinterpret_cast<uintptr_t>(static_cast<void const*>(this));
-          uintptr_t const recent  = reinterpret_cast<uintptr_t>(array_internals::recent);
+        element() {}
+      } nondata[N];
+      T data   [N];
 
-          // ...
-          NULL == value ? std::printf(array_internals::count == minimum - 1u ? "+" : "-") : std::printf("%i", (int) *value);
-          if (array_internals::count == minimum - 1u) {
-            // PUT INDEX TO HEAP MEMORY HERE @ `storage* this`
-            (void) std::printf("{%zu}" "\r\n", array_internals::count);
-          } else { std::printf("{%zu}" " ", array_internals::count); }
-          std::printf(minimum == 1 ? "\r\n" : "");
+      automatic() : nondata() {}
+    } automatic;
+    T *dynamic;
+  }           elements;
+  std::size_t length;
 
-          if (array_internals::count >= minimum - 0u || sizeof(base) < (address < recent ? recent - address : address - recent))
-          array_internals::count = 0u;
+  /* ... */
+  array() noexcept : elements(), length(0u) {}
 
-          array_internals::count++;
-          array_internals::recent = this;
-        }
+  // ...
+  bool add(T&& element) noexcept {
+    if (this -> length < N) {
+      (void) ::new (&this->elements.automatic.nondata[this->length++].value) T(element); // activate union member
+      return true;
+    }
 
-      public:
-        storage()                  : _() { this -> initialize(NULL); }
-        storage(base const& value) : _() { this -> initialize(reinterpret_cast<base const*>(&reinterpret_cast<unsigned char const&>(value))); }
-    };
+    return false;
+  }
 
-    static union storage const *recent;
+  T* data() const {
+    return this->data(std::index_sequence<0u>{}, std::make_index_sequence<N>());
+  }
 
-  public:
-    union storage _[minimum];
-    bool add(union param const& element1 = param(), union param const& element2 = param(), union param const& element3 = param());
-};
+  template <std::size_t... Is>
+  T* data(std::index_sequence<Is...>, std::index_sequence<Is...>) const {
+    T elements[sizeof...(Is)] = {this->elements.automatic.nondata[Is].value...};
 
-template <typename base>
-struct array<base, 0u> : array_internals {
-  public: union { struct array_internals __; base *_; };
+    for (union elements::automatic::element const &element : this->elements.automatic.nondata)
+    element.value.~T();
+
+    return ::new ((void*) &this->elements.automatic.nondata) T[sizeof...(Is)] {elements[Is]...};
+  }
+
+  template <std::size_t... Is, std::size_t... Js>
+  T* data(std::index_sequence<Is...> sequence, std::index_sequence<Js...> sentinel) const {
+    return this->length == sizeof...(Is) ? this->data(sequence, sequence) : this->data(std::index_sequence<Is..., sizeof...(Is)>{}, sentinel);
+  }
 };
 
 /* Main */
-int main(void) {
-  array<long, 0u> a;
-  array<long, 3u> b[] = {{1, 2, 3}, {4, 5, 6}};
-  array<unsigned char, 3u> c[] = {{10, 20, 30}, {40, 50, 60}, {70}, {80}};
-  array<short, 1u> d[] = {{100}, {200}, {300}};
-  array<long long, 1u> e[] = {{400}, {500}, {600}};
+int main(void) noexcept {
+  struct loud {
+    int value;
 
-  (void) a;
-  (void) b;
-  (void) c;
-  (void) d;
-  (void) e;
+    // loud()    : value(-1) { std::printf("<.>" " "); } // uninitialized
+    loud(int) : value(+1) { std::printf("<!>" " "); } // initialized
+  };
+
+  array<struct loud, 3u> array = {};
+
+  std::printf("\n" "[add] : "); array.add(1);
+  std::printf("\n" "[data]: "); array.data();
 }
